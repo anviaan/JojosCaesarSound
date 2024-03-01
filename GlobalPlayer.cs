6d90c6232615dae19ea6660dd78541945bd1eebe
andre-carbajal <carbajalvargasandre@gmail.com>
2024-02-29 22:45:43 -0500

    mod is done

--- GlobalPlayer.cs
+++ GlobalPlayer.cs
@@ -1,5 +1,5 @@
-﻿using Terraria;
-using Terraria.Audio;
+﻿using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -7,13 +7,17 @@ namespace JojosCaesarSound;
 
 public class GlobalPlayer : ModPlayer
 {
-    public override void OnHitByProjectile(Projectile proj, Player.HurtInfo hurtInfo)
+    public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genDust,
+        ref PlayerDeathReason damageSource)
     {
-        if (proj.type == ProjectileID.Boulder)
+        if (damageSource.SourceProjectileType == ProjectileID.Boulder)
         {
             SoundStyle caesarSound = new SoundStyle("JojosCaesarSound/Sounds/caesar")
-                { Volume = 2.0f, MaxInstances = 1 };
+                { Volume = 3.0f, MaxInstances = 1 };
             SoundEngine.PlaySound(caesarSound, Player.position);
+        }
+        {
+            return base.PreKill(damage, hitDirection, pvp, ref playSound, ref genDust, ref damageSource);
         }
     }
 }
