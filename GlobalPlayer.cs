7de2e6c8c62009be066370fca608e2930da84442
andre-carbajal <carbajalvargasandre@gmail.com>
2024-02-29 19:03:23 -0500

    syntax fix

--- GlobalPlayer.cs
+++ GlobalPlayer.cs
@@ -5,18 +5,15 @@ using Terraria.ModLoader;
 
 namespace JojosCaesarSound;
 
-public class GlobalPlayer: ModPlayer
+public class GlobalPlayer : ModPlayer
 {
     public override void OnHitByProjectile(Projectile proj, Player.HurtInfo hurtInfo)
     {
         if (proj.type == ProjectileID.Boulder)
         {
-            SoundStyle caesarSound = new SoundStyle("JojosCaesarSound/Sounds/caesar") with
-            {
-                Volume = 2.0f,
-                MaxInstances = 1
-            };
-            SoundEngine.PlaySound( caesarSound, Player.position);
+            SoundStyle caesarSound = new SoundStyle("JojosCaesarSound/Sounds/caesar")
+                { Volume = 2.0f, MaxInstances = 1 };
+            SoundEngine.PlaySound(caesarSound, Player.position);
         }
     }
 }
